b242434c02508bd9f49a15bc158dbb66484f785b
Oğulcan <ogulcan.gulfidan@gmail.com>
2021-09-03 19:14:19 +0300

    ReCapCarUser
    Projeye usert ve rental claslları ekledik

---
+++ ReCapCarProject/Business/Concrete/Usermanager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class Usermanager : IUserService
+    {
+        IUserDal _userDal;
+
+        public Usermanager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public IResult Add(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<User> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ReCapCarProject/Business/Constants/Messages.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public class Messages
+    {
+        internal static string ProductsListed="kayıtlar Listelendi";
+        internal static string ProductAdded="Veri Eklendi";
+        internal static IResult ProductLoad;
+        internal static string ProductUpdated="Ürün Güncellendi";
+        internal static string ProductDeleted="Ürün Başarıyla Silindi";
+        internal static string GetProductById="Id ye göre ürün bulundu";
+        internal static string Fail="İşlem Başarısız oldu";
+        internal static string RentalAdded="Araba kiralandı";
+        internal static string RentalListed="Kiralık Arabalar Listelendi";
+    }
+}
